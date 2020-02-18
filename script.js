cfbae26add8033d340bb9f4474367a79c60b4109
alexduboisky <alexduboisky@yandex.ru>
2020-02-18 16:06:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    let currentWeather = document.querySelector('.current_weather')
     let weatherCity = document.querySelector('.weather_city')
     let weatherTime = document.querySelector('.weather_time')
     let weatherIcon = document.querySelector('#weather_icon')
@@ -9,82 +10,140 @@ document.addEventListener('DOMContentLoaded', function () {
     let weatherWindSpeed = document.querySelector('.weather_wind__speed')
     let forecast = document.querySelector('.forecast')
     let container = document.querySelector('.container')
-    let dt
+    let dt;
 
-    let xhr = new XMLHttpRequest();
-    const API_KEY = '84d6ecb8cce7742b9bed2b6595991fb7';
-    xhr.open('GET', 'https://api.openweathermap.org/data/2.5/forecast?q=Minsk&units=metric&appid=' + API_KEY);
-    xhr.responseType = 'json';
 
-    xhr.onload = function () {
-        if (xhr.status != 200) {
-            alert('Ошибка: ' + xhr.status);
-            container.style.backgroundImage = `url('img/bg.jpg')`
-            return;
-        } else {
-            let response = xhr.response;
-            dt = new Date(response.list[0].dt * 1000)
+    currentWeather.style.background = `url('img/Spin.gif') center center no-repeat`
 
-            switch (true) {
-                case (response.list[0].weather[0]['main'] == 'Snow'):
-                    container.style.backgroundImage = `url('img/snow.gif')`
-                    break;
-                case (response.list[0].weather[0]['main'] == 'Drizzle'):
-                    container.style.backgroundImage = `url('img/drizzle.gif')`
-                    break;
-                case (response.list[0].weather[0]['main'] == 'Rain'):
-                    container.style.backgroundImage = `url('img/rain.gif')`
-                    break;
-                case (response.list[0].weather[0]['main'] == 'Mist'):
-                    container.style.backgroundImage = `url('img/mist.gif')`
-                    break;
-                case (response.list[0].weather[0]['main'] == 'Clouds'):
-                    container.style.backgroundImage = `url('img/bg_gif.gif')`
-                    break;
-                case (response.list[0].weather[0]['main'] == 'Thunderstorm'):
-                    container.style.backgroundImage = `url('img/thunderstorm.gif)`
-                    break;
-                default:
-                    container.style.backgroundImage = `url('img/bg.jpg')`
-                    break;
+    ymaps.ready(function () {
+        let location = ymaps.geolocation.get({
+            provider: 'browser'
+        });
+        location.then(
+            function (result) {
+
+                let myPlacemark,
+                myMap = new ymaps.Map("first_map", {
+                    center: [result.geoObjects.position[0], result.geoObjects.position[1]],
+                    zoom: 15
+                });
+                myMap.geoObjects
+                    .add(new ymaps.Placemark([result.geoObjects.position[0], result.geoObjects.position[1]], {
+                        preset: 'islands#icon',
+                        iconColor: '#0095b6'
+                    }))
+                getWeather(result.geoObjects);
+                myMap.events.add('click',function(e){
+                    result = e.get('coords');
+                    clearDivs();
+                    getWeather(result)
+                    if(myPlacemark){
+                        myPlacemark.geometry.setCoordinates(result);
+                    }
+                    else{
+                        myPlacemark = createPlacemark(result);
+                        myMap.geoObjects.add(myPlacemark);
+                    }
+                })
+                function createPlacemark(result) {
+                    return new ymaps.Placemark(result,{
+                        preset: 'islands#violetDotIconWithCaption',
+                        draggable: true
+                    });
+                }
+                
             }
+        );
 
-            setMainWeather(response.city['name'], response.city['country'], dt.toLocaleTimeString().slice(0, 5), response.list[0].weather[0].icon, Math.round(response.list[0].main['temp']), Math.round(response.list[0].main['feels_like']), response.list[0].wind['deg'], Math.round(response.list[0].wind['speed']))
-            for (let i = 0; i < Object.keys(response.list).length; i += 8) {
-                dt = new Date(response.list[i].dt * 1000)
-                forecast.appendChild(setSecondWeather(dt, response.list[i].weather[0].icon, Math.round(response.list[i].main['temp'])))
+
+    });
+
+    function getWeather(position) {
+        let lat;
+        let lon;
+        if(position[0]!= undefined){
+           lat = position[0];
+           lon = position[1];
+        }
+        else{
+            lat = position.position[0];
+            lon = position.position[1];
+        }
+
+        
+        let xhr = new XMLHttpRequest();
+        let xhrCurrent = new XMLHttpRequest();
+        const API_KEY = '84d6ecb8cce7742b9bed2b6595991fb7';
+        xhr.open('GET', `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`);
+        xhr.responseType = 'json';
+        xhrCurrent.open('GET', `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`)
+        xhrCurrent.responseType = 'json';
+        xhr.onload = function () {
+            if (xhr.status != 200) {
+                alert('Ошибка: ' + xhr.status);
+                container.style.backgroundImage = `url('img/bg.jpg')`
+                return;
+            } else {
+                let response = xhr.response;
+                dt = new Date(response.list[0].dt * 1000)
+                for (let i = 0; i < Object.keys(response.list).length; i += 8) {
+                    dt = new Date(response.list[i].dt * 1000)
+                    forecast.appendChild(setSecondWeather(dt, response.list[i].weather[0].icon, Math.round(response.list[i].main['temp'])))
+                }
             }
         }
-    };
+        xhrCurrent.onload = function () {
+            if (xhrCurrent.status != 200) {
+                alert('Ошибка: ' + xhrCurrent.status);
+                container.style.backgroundImage = `url('img/bg.jpg')`
+                return;
+            } else {
+                currentWeather.style.background = `rgba(240, 248, 255,0.5)`
+                let response = xhrCurrent.response;
+                dt = new Date(response.dt * 1000)
+                container.style.backgroundImage = `url('img/${response.weather[0]['main']}.gif')`;
+
+                setMainWeather(response.name, response.sys['country'], dt.toLocaleTimeString().slice(0, 5), response.weather[0].icon, Math.round(response.main['temp']), Math.round(response.main['feels_like']), response.wind['deg'], Math.round(response.wind['speed']))
+            }
+        }
+        xhrCurrent.onerror = function () {
+            alert('Error')
+        };
+        xhr.onerror = function () {
+            alert('Error')
+        };
+        xhr.send();
+        xhrCurrent.send();
+    }
 
     function setMainWeather(city, country, time, icon, temp, feels, direction, speed) {
         switch (true) {
-            case (direction == 0):
+            case (direction >=337.6 && direction <=22.5):
                 weatherWindDirection.innerHTML = `<i class="far fa-compass"></i> North`;
                 break;
-            case (direction > 0 && direction < 90):
+            case (direction >=22.6 && direction <=67.5):
                 weatherWindDirection.innerHTML = `<i class="far fa-compass"></i> North-East`;
                 break;
-            case (direction == 90):
+            case (direction >=67.6 && direction <=112.5):
                 weatherWindDirection.innerHTML = `<i class="far fa-compass"></i> East`;
                 break;
-            case (direction > 91 && direction < 180):
+            case (direction >=112.6 && direction <=157.5):
                 weatherWindDirection.innerHTML = `<i class="far fa-compass"></i> South-East`;
                 break;
-            case (direction == 180):
+            case (direction >=157.6 && direction <=202.5):
                 weatherWindDirection.innerHTML = `<i class="far fa-compass"></i> South`;
                 break;
-            case (direction > 180 && direction < 270):
+            case (direction >=202.6 && direction <=247.5):
                 weatherWindDirection.innerHTML = `<i class="far fa-compass"></i> South-West`;
                 break;
-            case (direction == 270):
+            case (direction >=247.6 && direction <=292.5):
                 weatherWindDirection.innerHTML = `<i class="far fa-compass"></i> West`;
                 break;
-            case (direction > 271 && direction < 360):
+            case (direction >=292.6 && direction <=337.5):
                 weatherWindDirection.innerHTML = `<i class="far fa-compass"></i> North-West`;
                 break;
             default:
-                console.log('unknown')
+                weatherWindDirection.innerHTML = `<i class="far fa-compass"></i> Unknown`;
                 break;
         }
         weatherCity.textContent = `${city}, ${country}`;
@@ -109,8 +168,16 @@ document.addEventListener('DOMContentLoaded', function () {
         dayDiv.append(dateDiv, iconDiv, tempDiv)
         return dayDiv
     }
-    xhr.onerror = function () {
-        alert('Error')
-    };
-    xhr.send();
+
+    function clearDivs(){
+        weatherCity.innerHTML ='';
+        weatherTime.innerHTML ='';
+        weatherIcon.innerHTML ='';
+        weatherTemp.innerHTML ='';
+        weatherFeels.innerHTML ='';
+        weatherWindDirection.innerHTML ='';
+        weatherWindSpeed.innerHTML ='';
+        forecast.innerHTML ='';
+    }
+
 })
